3cb3d4f8c9ec39e30f6b5aade34e04f3bf13163a
Regis Leray <regis.leray@axon-id.com>
2014-02-12 18:04:49 -0500

    add empty test TODO

---
+++ src/test/java/org/rayjars/hibernate/JacksonUserTypeTest.java
@@ -0,0 +1,21 @@
+package org.rayjars.hibernate;
+
+import org.junit.Test;
+
+
+public class JacksonUserTypeTest {
+    @Test
+    public void testConvertJsonToObject() throws Exception {
+
+    }
+
+    @Test
+    public void testConvertObjectToJson() throws Exception {
+
+    }
+
+    @Test
+    public void testDeepCopy() throws Exception {
+
+    }
+}
